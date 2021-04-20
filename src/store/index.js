5c539efe63fdf0cdefdcc01a2e862ad9d72fc2ac
Anthony Nikolaidis <a.nikolaidis@student.tudelft.nl>
2021-04-21 00:09:43 +0300

    Set-up state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import moment from 'moment'
 
 Vue.use(Vuex)
 
 function updateTime() {
     let currentDate = new Date()
-    let day = currentDate.getDay()
-    let date = currentDate.getDate()
-    let month = currentDate.getMonth()
+    let formattedDate = moment(currentDate).format('ddd, MMM D')
 
     let seconds = currentDate.getSeconds()
     let minutes = currentDate.getMinutes()
     let hour = currentDate.getHours()
-
-    let days = [
-        {id: 0, day: 'Sunday'},
-        {id: 1, day: 'Monday'},
-        {id: 2, day: 'Tuesday'},
-        {id: 3, day: 'Wednesday'},
-        {id: 4, day: 'Thursday'},
-        {id: 5, day: 'Friday'},
-        {id: 6, day: 'Saturday'}
-    ]
-
-    let months = [
-        {id: 0, month: 'January'},
-        {id: 1, month: 'February'},
-        {id: 2, month: 'March'},
-        {id: 3, month: 'April'},
-        {id: 4, month: 'May'},
-        {id: 5, month: 'June'},
-        {id: 6, month: 'July'},
-        {id: 7, month: 'August'},
-        {id: 8, month: 'September'},
-        {id: 9, month: 'October'},
-        {id: 10, month: 'November'},
-        {id: 11, month: 'December'}
-    ]
-
-    day = days.find(d => d.id === day).day.substring(0, 3)
-    month = months.find(m => m.id === month).month.substring(0, 3)
 
     if (minutes < 10) {
         minutes = '0' + minutes
@@ -59,36 +30,181 @@ function updateTime() {
 
     if (hour === 0) {
         hour = 12
-    }
-    else if (hour > 12) {
+    } else if (hour > 12) {
         hour -= 12
     }
 
-    // let time = hour + ':' + minutes + ':' + seconds + ' ' + pmAm
     let time = hour + ':' + minutes + ' ' + pmAm
 
-    return day + ', ' + date + ' ' + month + ' – ' + time
+    return formattedDate + ' – ' + time
 }
 
 setInterval(() => {
     store.dispatch('updateDate').then(r => r)
 }, 1000)
 
+export function formatTime(time) {
+    if (time) {
+        let timeComponents = time.split(':')
+        let hours = parseInt(timeComponents[0])
+        let minutes = timeComponents[1]
+        let formattedTime = ''
+
+        let pmAm = ''
+
+        if (hours > 11) {
+            pmAm = 'PM'
+        } else {
+            pmAm = 'AM'
+        }
+
+        if (hours === 0) {
+            hours = 12
+        } else if (hours > 12) {
+            hours -= 12
+        }
+
+        formattedTime += hours
+
+        if (minutes !== '00') {
+            formattedTime += ':' + minutes
+        }
+
+        formattedTime += ' ' + pmAm
+
+        return formattedTime
+    }
+
+    return ''
+}
+
+export function getFormattedDate(date, time) {
+
+    let string = null
+
+    if (date !== null) {
+        if (parseInt(date.split('-')[2]) - new Date().getDate() === 1) {
+            string = 'Tomorrow'
+        } else if (parseInt(date.split('-')[2]) - new Date().getDate() === 0) {
+            string = 'Today'
+        } else if (parseInt(date.split('-')[2]) - new Date().getDate() === -1) {
+            string = 'Yesterday'
+        } else {
+            string = moment(date).format('ddd, MMM D')
+        }
+
+        if (time !== null) {
+            string += ', ' + formatTime(time)
+        }
+    }
+
+    return string
+}
+
 export const store = new Vuex.Store({
     state: {
         tasks: [],
-        date: ''
+        labels: [
+            {header: 'Select a label or create one'}
+        ],
+        date: '',
+        form: {show: false, type: null},
+        showCompleted: false,
+        taskToEdit: false,
+        currentTask: {
+            id: null,
+            title: '',
+            done: false,
+            details: '',
+            dueDate: null,
+            dueTime: null,
+            labels: []
+        }
     },
     mutations: {
+        sortByDueDate(state) {
+            state.tasks.sort(function (a, b) {
+                let dateA = 0
+                let dateB = 0
+
+                if (a.dueDate) {
+                    let stringDateA = a.dueDate + 'T'
+                    if (a.dueTime) {
+                        stringDateA += a.dueTime + 'Z'
+                    } else {
+                        stringDateA += '00:00Z'
+                    }
+                    dateA = new Date(stringDateA)
+                }
+
+                if (b.dueDate) {
+                    let stringDateB = b.dueDate + 'T'
+                    if (b.dueTime) {
+                        stringDateB += b.dueTime + 'Z'
+                    } else {
+                        stringDateB += '00:00Z'
+                    }
+                    dateB = new Date(stringDateB)
+                }
+
+                return dateA - dateB
+            })
+        },
+
+        sortByLabel(state) {
+            state.tasks.sort(function (a, b) {
+                if (a.labels.length !== 0 && b.labels.length !== 0) {
+                    if (a.labels[0].text > b.labels[0].text) {
+                        return 1
+                    } else {
+                        return -1
+                    }
+                } else {
+                    if (a.labels.length === 0) {
+                        return -1
+                    } else if (b.labels.length === 0) {
+                        return 0
+                    }
+                    return 0
+                }
+            })
+        },
+
+        updateCurrentTaskId(state, id) {
+            state.currentTask.id = id
+        },
+        updateCurrentTaskTitle(state, title) {
+            state.currentTask.title = title
+        },
+        updateCurrentTaskDetails(state, details) {
+            state.currentTask.details = details
+        },
+        updateCurrentTaskDueDate(state, date) {
+            state.currentTask.dueDate = date
+        },
+        updateCurrentTaskDueTime(state, time) {
+            state.currentTask.dueTime = time
+        },
+        updateCurrentTaskLabels(state, labels) {
+            state.currentTask.labels = labels
+        },
         updateDate(state) {
             state.date = updateTime()
         },
-        addTask(state, task) {
-            if (state.tasks === null) {
-                state.tasks = []
+        addTask(state) {
+            if (state.tasks == null) {
+              state.tasks = []
             }
-
-            state.tasks.push(task)
+            state.tasks.unshift(state.currentTask)
+            state.currentTask = {
+                id: null,
+                title: '',
+                done: false,
+                details: '',
+                dueDate: null,
+                dueTime: null,
+                labels: []
+            }
             localStorage.setItem('tasks', JSON.stringify(state.tasks))
         },
         deleteTask(state, id) {
@@ -100,8 +216,69 @@ export const store = new Vuex.Store({
             task.done = !task.done;
             localStorage.setItem('tasks', JSON.stringify(state.tasks))
         },
+        editTask(state) {
+            let taskIndex = state.tasks.findIndex(t => t.id === state.currentTask.id);
+            state.tasks[taskIndex] = state.currentTask
+            state.currentTask = {
+                id: null,
+                title: '',
+                done: false,
+                details: '',
+                dueDate: null,
+                dueTime: null,
+                labels: []
+            }
+            localStorage.setItem('tasks', JSON.stringify(state.tasks))
+        },
+        setTaskToEdit(state, task) {
+            if (task === null) {
+                state.taskToEdit = false
+            } else {
+                state.taskToEdit = true
+                state.currentTask = state.tasks.find(t => t.id === task.id)
+            }
+        },
+        stopEditing(state) {
+            state.currentTask = {
+                id: null,
+                title: '',
+                done: false,
+                details: '',
+                dueDate: null,
+                dueTime: null,
+                labels: []
+            }
+        },
+        appendToLabels(state, label) {
+          state.labels.push(label)
+        },
+        removeLabel(state, label) {
+            let deleteIndex = state.labels.findIndex(i => i === label)
+            state.labels.splice(deleteIndex, 1)
+
+            for (let i = 0; i < state.tasks.length; i++) {
+                state.tasks[i].labels = state.tasks[i].labels.filter(l => l !== label)
+            }
+
+            localStorage.setItem('labels', JSON.stringify(state.labels))
+        },
+        setForm(state, form) {
+            state.form = form
+        },
+        setShowCompleted(state, boolean) {
+            if (state.tasks.filter(t => t.done === true).length === 0) {
+                state.showCompleted = false
+            } else {
+                state.showCompleted = boolean
+            }
+        },
         initialiseStore(state) {
             state.tasks = JSON.parse(localStorage.getItem('tasks'))
+            if (JSON.parse(localStorage.getItem('labels'))) {
+                state.labels = JSON.parse(localStorage.getItem('labels'))
+            } else {
+                localStorage.setItem('labels', JSON.stringify(state.labels))
+            }
         }
     },
     actions: {
@@ -111,11 +288,23 @@ export const store = new Vuex.Store({
         addTask({commit}, task) {
             commit('addTask', task)
         },
-        deleteTask({commit}, task) {
-            commit('deleteTask', task)
+        deleteTask({commit}, id) {
+            commit('deleteTask', id)
         },
         doneTask({commit}, task) {
             commit('doneTask', task)
+        },
+        editTask({commit}, task) {
+            commit('editTask', task)
+        },
+        setTaskToEdit({commit}, task) {
+            commit('setTaskToEdit', task)
+        },
+        setForm({commit}, form) {
+            commit('setForm', form)
+        },
+        setLabels({commit, labels}) {
+            commit('setLabels', labels)
         }
     },
     modules: {},
@@ -123,8 +312,17 @@ export const store = new Vuex.Store({
         getTasks(state) {
             return state.tasks
         },
+        getLabels(state) {
+            return state.labels
+        },
         getDate(state) {
             return state.date
+        },
+        getForm(state) {
+            return state.form
+        },
+        getTaskToEdit(state) {
+            return state.taskToEdit
         }
     }
 })
